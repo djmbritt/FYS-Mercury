00f0771ebcb338a4499742ec670db01477d941ff
josenml <joseniem@yahoo.com>
2017-11-02 08:53:54 +0100

    ?

---
+++ Mercury/src/main/java/hva/fys/mercury/MainApp.java
@@ -0,0 +1,40 @@
+package hva.fys.mercury;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("Mercury");
+        stage.setFullScreen(Boolean.TRUE);
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
