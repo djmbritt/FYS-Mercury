695bf521ab00f0bbb777fa2633f6bfba53656d5d
josenml <joseniem@yahoo.com>
2017-11-02 19:15:34 +0100

    sidebar
    methods toegevoegd voor de navigatieknoppen

--- Mercury/src/main/java/hva/fys/mercury/FXMLController.java
+++ Mercury/src/main/java/hva/fys/mercury/FXMLController.java
@@ -4,17 +4,40 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable; 
+import javafx.fxml.Initializable;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
-    private void RegistreerGevondenbagage(ActionEvent event) {
-        System.out.println("registreer bagage geopend "); 
+    private void openRegistreerGevondenbagage(ActionEvent event) {
+        System.out.println("registreer bagage geopend ");
+    }
+
+    @FXML
+    private void openRegistreerVerlorenbagage(ActionEvent event) {
+        System.out.println("verloren bagage geopend ");
+    }
+
+    @FXML
+    private void openDashboard(ActionEvent event) {
+        System.out.println("dashboard geopend ");
+    }
+
+    @FXML
+    private void openBagageZoeken(ActionEvent event) {
+        System.out.println("bagage zoeken geopend ");
     }
     
+   @FXML
+    private void openManagerOverzicht(ActionEvent event) {
+        System.out.println("manager  geopend ");
+    }
+     @FXML
+    private void logout(ActionEvent event) {
+        System.out.println("iutloggen ");
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
