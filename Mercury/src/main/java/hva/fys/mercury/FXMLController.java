6fc5ee6c490b0ff6f940a000c120d2f484a74b3d
josenml <joseniem@yahoo.com>
2017-11-02 18:32:14 +0100

    nonsens
    oke xml moet in zelfde source map for now

--- Mercury/src/main/java/hva/fys/mercury/FXMLController.java
+++ Mercury/src/main/java/hva/fys/mercury/FXMLController.java
@@ -4,18 +4,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable; 
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void RegistreerGevondenbagage(ActionEvent event) {
+        System.out.println("registreer bagage geopend "); 
     }
     
     @Override
--- Mercury/src/main/java/hva/fys/mercury/MainApp.java
+++ Mercury/src/main/java/hva/fys/mercury/MainApp.java
@@ -16,23 +16,15 @@ public class MainApp extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
         
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
+        scene.getStylesheets().add("/styles/Styles.css"); 
         
         stage.setTitle("Mercury");
-        stage.setFullScreen(Boolean.TRUE);
+       // stage.setFullScreen(Boolean.TRUE);
         stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
