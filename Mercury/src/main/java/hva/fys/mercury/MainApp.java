a3e9f6dd4d0550b8b0708412c69db105945a6993
djmbritt <djmb102@gmail.com>
2017-11-10 11:44:52 +0100

    added app-icon

--- Mercury/src/main/java/hva/fys/mercury/MainApp.java
+++ Mercury/src/main/java/hva/fys/mercury/MainApp.java
@@ -5,22 +5,23 @@ import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
-
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css"); 
-        
+        scene.getStylesheets().add("/styles/Styles.css");
+
         stage.setTitle("Mercury");
-       // stage.setFullScreen(Boolean.TRUE);
-       stage.setResizable(false);
+        stage.getIcons().add(new Image("/images/corendon_icon.png"));
+        // stage.setFullScreen(Boolean.TRUE);
+        stage.setResizable(false);
         stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.setScene(scene);
         stage.show();
