71c4f119ec4c9483ac133b43492c560a5ae86ae6
JoseNML <33283355+JoseNML@users.noreply.github.com>
2017-11-12 12:30:48 +0100

    maximazied
    het word maximazied in plaats van fullscreen

--- Mercury/src/main/java/hva/fys/mercury/MainApp.java
+++ Mercury/src/main/java/hva/fys/mercury/MainApp.java
@@ -4,24 +4,21 @@ import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.Scene; 
 import javafx.stage.Stage;
-
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css"); 
-        
+        scene.getStylesheets().add("/styles/Styles.css");
+
         stage.setTitle("Mercury");
-       // stage.setFullScreen(Boolean.TRUE);
-       stage.setResizable(false);
-        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        stage.setMaximized(true);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
