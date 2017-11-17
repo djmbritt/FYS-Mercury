1b9c5fb983ec494112d903d1b10949671a1ac6f1
JoseNML <33283355+JoseNML@users.noreply.github.com>
2017-11-17 21:09:46 +0100

    GOSE
    sql connector ingesteld

---
+++ Mercury/src/main/java/hva/fys/mercury/controllers/FormulierController.java
@@ -0,0 +1,13 @@
+package hva.fys.mercury.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FormulierController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
