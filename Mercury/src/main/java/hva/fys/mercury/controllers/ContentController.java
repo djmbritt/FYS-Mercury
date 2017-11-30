966412a2201686ea9069e1682d717c8e01bcf811
JoseNML <33283355+JoseNML@users.noreply.github.com>
2017-11-30 21:17:28 +0100

    g

--- Mercury/src/main/java/hva/fys/mercury/controllers/ContentController.java
+++ Mercury/src/main/java/hva/fys/mercury/controllers/ContentController.java
@@ -19,9 +19,7 @@ import javafx.stage.Stage;
 
 public class ContentController implements Initializable {
 
-    
-    
-
+     
     @FXML
     private StackPane workspace;
 
@@ -100,16 +98,7 @@ public class ContentController implements Initializable {
         workspace.getChildren().setAll(pane);
     }
     
-    @FXML
-    private void openBagageDetails(ActionEvent event) {
-        System.out.println("Bagage details geopend ");
-        Parent pane = loadFXMLFile("/fxml/bagageDetails.fxml");
-//https://stackoverflow.com/questions/21966441/gettext-method-returning-null-even-though-a-value-is-entered-by-user
-        System.out.println(pane);
-        System.out.println(workspace);
-        workspace.getChildren().clear();
-        workspace.getChildren().setAll(pane);
-    }
+ 
 
     @FXML
     private void logoutAction(ActionEvent event) {
