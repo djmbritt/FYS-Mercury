e98ae0b70c57a2a9453e31333f21b425c73b3682
Mitchell7112 <Mitchellwan28@gmail.com>
2017-11-30 16:08:12 +0100

    dsads

--- Mercury/src/main/java/hva/fys/mercury/controllers/RegistreerVermistController.java
+++ Mercury/src/main/java/hva/fys/mercury/controllers/RegistreerVermistController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hva.fys.mercury.controllers;
 
 import hva.fys.mercury.models.Bagage;
