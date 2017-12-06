5ff236dd83e4633c4b284276b917644c4c756b1e
JoseNML <33283355+JoseNML@users.noreply.github.com>
2017-12-06 13:06:20 +0100

    gf

--- Mercury/src/main/java/hva/fys/mercury/controllers/BagageResultatenController.java
+++ Mercury/src/main/java/hva/fys/mercury/controllers/BagageResultatenController.java
@@ -17,8 +17,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.cell.PropertyValueFactory; 
 
 public class BagageResultatenController implements Initializable {
 
--- Mercury/src/main/java/hva/fys/mercury/controllers/DashboardController.java
+++ Mercury/src/main/java/hva/fys/mercury/controllers/DashboardController.java
@@ -1,19 +1,10 @@
 package hva.fys.mercury.controllers;
- 
+
 import hva.fys.mercury.DAO.BagageDAO;
-import hva.fys.mercury.DAO.DatabaseManager;
-import hva.fys.mercury.DAO.GebruikerDAO;
-import hva.fys.mercury.MainApp;
 import hva.fys.mercury.models.Bagage;
-import hva.fys.mercury.models.Gebruiker;
 import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Struct;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -23,15 +14,14 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class DashboardController implements Initializable {
-    private int Percentage;
-    
-    private static final DatabaseManager DB_MANAGER = new DatabaseManager(MainApp.DATABASE_NAME);
+
+    @FXML
+    Button refresh;
     @FXML
     private PieChart pieChart;
 
@@ -44,31 +34,24 @@ public class DashboardController implements Initializable {
     private LineChart<?, ?> lineChart;
 
     @FXML
-    private Label chartLabel;
-    
-    @FXML
-    private TableView bagageTable;
+    TableView mostRecentTable;
 
-    @FXML
-    private Label resultaten;
-    private final ObservableList<Bagage> bagageLijst = FXCollections.observableArrayList();
-
-    private ParentControllerContext parentController;
-
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        fillChart();
         initializeLineChart();
         initializePieChart();
-        initializeTable();
-        this.fillTable(bagageLijst);
-    }            
+    }
 
-    public void fillTable(List<Bagage> list) {
-        bagageLijst.addAll(bagageLijst);
-        bagageTable.setItems(bagageLijst);
-        for (int cnr = 0; cnr < bagageTable.getColumns().size(); cnr++) {
-            TableColumn tc = (TableColumn) bagageTable.getColumns().get(cnr);
+    private void fillChart() {
+        mostRecentTable.refresh();
+        List<Bagage> lijst = BagageDAO.getMeestRecenteVerloren(); 
+        ObservableList<Bagage> bagageLijst = FXCollections.observableArrayList();
+        bagageLijst.addAll(lijst);
+        mostRecentTable.setItems(bagageLijst);
+
+        for (int cnr = 0; cnr < mostRecentTable.getColumns().size(); cnr++) {
+            TableColumn tc = (TableColumn) mostRecentTable.getColumns().get(cnr);
             String propertyName = tc.getId();
 
             if (propertyName != null && !propertyName.isEmpty()) {
@@ -76,47 +59,35 @@ public class DashboardController implements Initializable {
                 System.out.println("Attached collumn " + propertyName + "in tableview to matching attribute.");
             }
         }
-        String resultFormat = "%d Gevonden resultaten.";
-        resultaten.setText(String.format(resultFormat, bagageLijst.size()));
+        
     }
 
-    public void refreshTable() {
-        bagageTable.refresh();
-    }
-    
-    private void initializeTable() {
-         BagageDAO.getRecentBagage(bagageLijst);
-    }
-    
-    private void initializeLineChart() { 
+    private void initializeLineChart() {
         XYChart.Series gevonden = new XYChart.Series();
         gevonden.setName("Gevonden");
         gevonden.getData().add(new XYChart.Data("1", 23));
         gevonden.getData().add(new XYChart.Data("2", 11));
         gevonden.getData().add(new XYChart.Data("3", 33));
         gevonden.getData().add(new XYChart.Data("4", 44));
-        
-         XYChart.Series verloren = new XYChart.Series();
-         verloren.setName("verloren");
+
+        XYChart.Series verloren = new XYChart.Series();
+        verloren.setName("verloren");
         verloren.getData().add(new XYChart.Data("1", 11));
         verloren.getData().add(new XYChart.Data("2", 22));
         verloren.getData().add(new XYChart.Data("3", 33));
         verloren.getData().add(new XYChart.Data("4", 41));
-        
+
         lineChart.getData().addAll(gevonden, verloren);
     }
-    
-    
+
     private void initializePieChart() {
         ObservableList<PieChart.Data> pieChartData
                 = FXCollections.observableArrayList(
-                        new PieChart.Data("Gevonden", BagageDAO.getStatusBagageSize("Gevonden")),
-                        new PieChart.Data("Verloren", BagageDAO.getStatusBagageSize("Verloren"))
+                        new PieChart.Data("Gevonden", 100),
+                        new PieChart.Data("Verloren", 200)
                 );
 
         pieChart.setTitle("All Time");
         pieChart.setData(pieChartData);
     }
-   
-
 }
