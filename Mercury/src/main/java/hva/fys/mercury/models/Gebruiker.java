98363841eb38e6e9b0be609f0f32da7922f71313
Mitchell7112 <Mitchellwan28@gmail.com>
2017-12-08 17:44:41 +0100

    Update

--- Mercury/src/main/java/hva/fys/mercury/models/Gebruiker.java
+++ Mercury/src/main/java/hva/fys/mercury/models/Gebruiker.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hva.fys.mercury.models;
 
 import hva.fys.mercury.DAO.GebruikerDAO;
 
 /**
- *
- * @author GG
+ * Deze model bevat alle gebruiker informatie.
+ * @author David Britt
  */
 public class Gebruiker {
 
